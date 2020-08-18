cc80af5930f00cbae309aa7e1b0431eca3aa4982
Saeed Rasooli <saeed.gnu@gmail.com>
2020-08-18 17:01:01 +0430

    add cmd/main.go

---
+++ cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/kapmahc/stardict"
+)
+
+func main() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	dicDir := path.Join(homeDir, ".stardict", "dic")
+	dics, err := stardict.Open(dicDir)
+	if err != nil {
+		panic(err)
+	}
+	for _, word := range os.Args[1:] {
+		for dicI, dic := range dics {
+			if dicI > 0 {
+				fmt.Printf("\n")
+			}
+			transList := dic.Translate(word)
+			if len(transList) > 0 {
+				fmt.Printf("--> query %#v from %s\n", word, dic.GetBookName())
+			}
+			for transI, trans := range transList {
+				if transI > 0 {
+					fmt.Printf("----------\n")
+				}
+				for _, part := range trans.Parts {
+					fmt.Printf("%v\n", strings.TrimSpace(string(part.Data)))
+				}
+				fmt.Printf("\n")
+			}
+		}
+	}
+}
