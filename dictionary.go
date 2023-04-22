9e7d2fa160ce2dcbfaaf29d3170d78fa628d9bb6
Saeed Rasooli <saeed.gnu@gmail.com>
2023-04-23 01:27:27 +0330

    move runWorkers func out of Dictionary struct

--- dictionary.go
+++ dictionary.go
@@ -96,49 +96,6 @@ func similarity(r1 []rune, r2 []rune, subtract uint8) uint8 {
 	return score - subtract
 }
 
-func (d *dictionaryImp) runWorkers(
-	N int,
-	workerCount int,
-	timeout time.Duration,
-	worker func(int, int) []*common.SearchResultLow,
-) []*common.SearchResultLow {
-	if workerCount < 2 {
-		return worker(0, N)
-	}
-	if N < 2*workerCount {
-		return worker(0, N)
-	}
-
-	ch := make(chan []*common.SearchResultLow, workerCount)
-
-	sender := func(start int, end int) {
-		ch <- worker(start, end)
-	}
-
-	step := N / workerCount
-	start := 0
-	for i := 0; i < workerCount-1; i++ {
-		end := start + step
-		go sender(start, end)
-		start = end
-	}
-	go sender(start, N)
-
-	results := []*common.SearchResultLow{}
-	timeoutCh := time.NewTimer(timeout)
-	for i := 0; i < workerCount; i++ {
-		select {
-		case wRes := <-ch:
-			results = append(results, wRes...)
-		case <-timeoutCh.C:
-			log.Println("Search Timeout")
-			return results
-		}
-	}
-
-	return results
-}
-
 // SearchFuzzy: run a fuzzy search with similarity scores
 // ranging from 140 (which means %70) to 200 (which means 100%)
 func (d *dictionaryImp) SearchFuzzy(
@@ -233,7 +190,7 @@ func (d *dictionaryImp) SearchFuzzy(
 	t1 := time.Now()
 	N := len(entryIndexes)
 
-	results := d.runWorkers(
+	results := runWorkers(
 		N,
 		workerCount,
 		timeout,
@@ -307,7 +264,7 @@ func (d *dictionaryImp) SearchStartWith(
 	t1 := time.Now()
 	N := len(entryIndexes)
 
-	results := d.runWorkers(
+	results := runWorkers(
 		N,
 		workerCount,
 		timeout,
@@ -348,7 +305,7 @@ func (d *dictionaryImp) searchPattern(
 	const minScore = uint8(140)
 
 	N := len(idx.entries)
-	return d.runWorkers(
+	return runWorkers(
 		N,
 		workerCount,
 		timeout,
